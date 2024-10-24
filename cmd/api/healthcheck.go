d968637d8e2765becf216a5c90213ed45ffa3a79
Nisvet Mujkic <nisvet.mujkic@outlook.com>
2024-10-24 21:32:25 +0200

    add progress

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
+	}
+
+	err := app.writeJSON(w, http.StatusOK, env, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem a could not process your request", http.StatusInternalServerError)
+	}
+}
