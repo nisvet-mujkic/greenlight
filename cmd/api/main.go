241cb2aca95b19c888fcfad6507a0e86ca58a133
Nisvet Mujkic <nisvet.mujkic@outlook.com>
2024-11-06 20:13:31 +0100

    get and create endpoints update

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	_ "github.com/lib/pq"
+	"greenlight.nisvetmujkic.com/internal/data"
 )
 
 const version = "1.0.0"
@@ -31,6 +32,7 @@ type config struct {
 type application struct {
 	config config
 	logger *slog.Logger
+	models data.Models
 }
 
 func main() {
@@ -66,6 +68,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
