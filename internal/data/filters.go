90d3bb90e491191efee74283cb8a905c47d22924
Nisvet Mujkic <nisvet.mujkic@outlook.com>
2024-11-10 20:13:52 +0100

    use full text search

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,10 @@
 package data
 
-import "greenlight.nisvetmujkic.com/internal/validator"
+import (
+	"strings"
+
+	"greenlight.nisvetmujkic.com/internal/validator"
+)
 
 type Filters struct {
 	Page         int
@@ -17,4 +21,22 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 
 	v.Check(validator.PermitedValue(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafeList {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
 }
